40ea55c394f38328fbe5517111540757160840cc
luigi <luigi.annovi@fermi.mo.it>
2019-04-05 15:09:40 +0200

    Primo commit del branch mqtt

---
+++ mqtt.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+class mqtt:
+    temperatura=0
+    listaTemp=[]
+    tempOra=0
+    tempMin=0
+    def __init__(self):
+        self.temperatura=0
+    def connect_and_take(self):
+        self.temperatura=os.system("mosquitto_sub -h \"broker.shiftr.io\" -u \"calvino00\" -P \"0123456789\" -t calvino-08/temperatura")
+        #Aggiunta a liste
+        self.listaTemp.append(self.temperatura)
+    def ultimaOra(self):
+        self.tempOra=0
+        for i in range(1,721):
+            self.tempOra=self.listaTemp[-i]+self.tempOra
+        self.tempOra=self.tempOra/720
+    def ultimoMinuto(self):
+        self.tempMin=0
+        for i in range(1,13):
+            self.tempMin=self.listaTemp[-i]+self.tempMin
+        self.tempMin=self.tempOra
+    def ultimiDieciMin(self):
+        self.tempDieci=0
+        for i in range(1,121):
+            self.tempDieci=self.listaTemp[-i]+self.tempDieci
+        self.tempDieci=self.tempDieci/120
+
+
+
