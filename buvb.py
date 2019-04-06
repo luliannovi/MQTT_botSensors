a5ceafc8e04f296e53dc39c0691fadd8b453dffc
riccardo2001 <48824164+riccardo2001@users.noreply.github.com>
2019-04-06 11:22:44 +0200

    Delete buvb.py

--- buvb.py
+++
@@ -1,9 +0,0 @@
-import pzgram
-pzgram.Bot("751724127:AAFlji-_pV1zwJfIG8ocwL9ei1DIMW7AVB8")
-button_list = [
-    InlineKeyboardButton("col1", callback_data=...),
-    InlineKeyboardButton("col2", callback_data=...),
-    InlineKeyboardButton("row 2", callback_data=...)
-]
-reply_markup = InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
-bot.send_message(..., "A two-column menu", reply_markup=reply_markup)
