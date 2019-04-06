b59dd7c503ffdc9d0bf106e78c09da147179e7f3
luigi <luigi.annovi@fermi.mo.it>
2019-04-06 11:42:20 +0200

    Commit 1.3 mqtt --> COMPLETATO

--- mqtt.py
+++ mqtt.py
@@ -1,5 +1,5 @@
 import paho.mqtt.client as mqtt
-class mqtt:
+class mqtt_bot:
     def __init__(self):
         self.temperatura=0
         self.temperatura = 0
@@ -10,27 +10,25 @@ class mqtt:
         self.cont1 = 0
         self.cont10 = 0
         self.contOra = 0
-        self.client = mqtt.Client(client_name="calvino00", protocol=mqtt.MQTTv311)
+        self.client = mqtt.Client()
     def connect_and_take(self):
-
+        self.client = mqtt.Client(protocol=mqtt.MQTTv311)
         self.client.username_pw_set(username="calvino00", password="0123456789")
-        self.client.on_connect = on_connect
-        self.client.on_subscribe = on_subscribe
-        self.client.on_message = on_message
+        self.client.on_connect = self.on_connect
+        #self.client.on_subscribe = self.on_subscribe
+        self.client.on_message = self.on_message
 
         self.client.connect(host='broker.shiftr.io', port=1883, keepalive=60)
         try:
             self.client.loop_forever()
         except KeyboardInterrupt:
             print()
-
-
     def on_connect(self, client, userdata, flags, rc):
         print('result from connect: {}'.format(mqtt.connack_string(rc)))
         self.client.subscribe('calvino-08/temperatura', qos=0)
 
-    def on_subscribe(client, userdata, mid, granted_qos):
-        print('subscribed topic with QoS: {}'.format(granted_qos[0]))
+    #def on_subscribe(client, userdata, mid, granted_qos):
+        #print('subscribed topic with QoS: {}'.format(granted_qos[0]))
 
     def on_message(self,client, userdata, msg):
         self.temperatura = str(msg.payload.decode("utf-8"))
