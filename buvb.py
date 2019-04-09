21793c65381ea7abb136fa007737e1479c7674f0
riccardo2001 <riklupo2001@gmail.com>
2019-04-09 09:02:02 +0200

    Controllare funzioni di assegnazione valori, non funziona

--- buvb.py
+++ buvb.py
@@ -1,9 +1,5 @@
 import pzgram
-pzgram.Bot("751724127:AAFlji-_pV1zwJfIG8ocwL9ei1DIMW7AVB8")
-button_list = [
-    InlineKeyboardButton("col1", callback_data=...),
-    InlineKeyboardButton("col2", callback_data=...),
-    InlineKeyboardButton("row 2", callback_data=...)
-]
-reply_markup = InlineKeyboardMarkup(util.build_menu(button_list, n_cols=2))
-bot.send_message(..., "A two-column menu", reply_markup=reply_markup)
+import telegram_bot
+telegram_bot.isnta(5)
+telegram_bot.unoraa(5)
+telegram_bot.bot.run()
